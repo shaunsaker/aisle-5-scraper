eaa46b17b9768e973a8ac5820e9a9e42c2114f4d
Shaun Saker <sakershaun@gmail.com>
2021-08-22 17:41:36 +0200

    getProducts

--- src/woolworths/getProducts/index.ts
+++ src/woolworths/getProducts/index.ts
@@ -1,4 +1,5 @@
-import { Category } from '../../firebase/models';
+import { getShopCategories } from '../../firebase/getShopCategories';
+import { Category, ShopIds } from '../../firebase/models';
 import { saveProducts } from '../../firebase/saveProducts';
 import { woolworthsBaseUrl } from '../models';
 import { parseProducts } from './parseProducts';
@@ -26,8 +27,7 @@ const scrapeAndSaveProducts = async (pageUrl: string, productCount: number) => {
 };
 
 const getProducts = async () => {
-  const categories: Category[] =
-    require('../getCategories/categories.json').slice(0, 1); //await getShopCategories(ShopIds.Woolworths);
+  const categories: Category[] = await getShopCategories(ShopIds.Woolworths);
 
   for (const category of categories) {
     const productCount = 0;
